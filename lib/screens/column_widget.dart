0bd4e428cd157abc789207ea4db939eafea74ccf
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-15 10:45:10 +0530

    Setting MainAxisAlignment of column to center

--- lib/screens/column_widget.dart
+++ lib/screens/column_widget.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-class RowWidget extends StatelessWidget {
-  const RowWidget({Key? key}) : super(key: key);
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Exploring Row Widget"),centerTitle: true,
+      appBar: AppBar(title: Text("Exploring Column Widget"),centerTitle: true,
           backgroundColor: Colors.black),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('Ananya',style: TextStyle(fontSize: 20),),
           Text('Puja',style: TextStyle(fontSize: 20),),
           Text('Aperna',style: TextStyle(fontSize: 20)),
         ],
       ),
-    );
+    );;
   }
 }
