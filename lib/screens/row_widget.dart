82f2a0973af66fe9e4f2bb43f0ab374433862d0e
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-18 10:09:36 +0530

    BottomNavigationBar

--- lib/screens/row_widget.dart
+++ lib/screens/row_widget.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 
-class RowWidget extends StatelessWidget {
-  const RowWidget({Key? key}) : super(key: key);
+class RowWidget extends StatefulWidget {
+
+  @override
+  State<RowWidget> createState() => _RowWidgetState();
+}
+
+class _RowWidgetState extends State<RowWidget> {
+  int selectedIndex=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        backgroundColor: Colors.deepPurple.shade50,
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.black,
+        onTap: (index){
+          //NOTE : whenever we call setState((){}) in a stateful widget, it rebuilds the whole UI
+          setState((){
+            selectedIndex=index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.add_shopping_cart),label: "List"),
+        ],
+      ),
       appBar: AppBar(
           //NOTE : Title parameter can take any widget
           title: Text("My App"),
