d94bd659ab16dbae7be57a9d7170ad7ec71ee59a
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-20 09:46:41 +0530

    functioning of checkedbox and showFloatingButton

--- lib/screens/row_widget.dart
+++ lib/screens/row_widget.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class RowWidget extends StatefulWidget {
-
   @override
   State<RowWidget> createState() => _RowWidgetState();
 }
 
 class _RowWidgetState extends State<RowWidget> {
   //NOTE : selectedIndex=0 means it will start from 0th index,i.e., Home
-  int selectedIndex=0;
+  int selectedIndex = 0;
+  bool showFloatingButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +18,17 @@ class _RowWidgetState extends State<RowWidget> {
         backgroundColor: Colors.deepPurple.shade50,
         selectedItemColor: Colors.teal,
         unselectedItemColor: Colors.black,
-        onTap: (index){
+        onTap: (index) {
           //NOTE : whenever we call setState((){}) in a stateful widget, it rebuilds the whole UI
-          setState((){
-            selectedIndex=index;
+          setState(() {
+            selectedIndex = index;
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
-          BottomNavigationBarItem(icon: Icon(Icons.add_shopping_cart),label: "List"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_shopping_cart), label: "List"),
         ],
       ),
       appBar: AppBar(
@@ -46,24 +47,43 @@ class _RowWidgetState extends State<RowWidget> {
                 })
           ],
           backgroundColor: Colors.black),
-      floatingActionButton: Container(height: 60,width: 100,
-      alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.shopping_cart,color: Colors.white,),
-          SizedBox(width: 4,),
-          Text('Cart',
-            style: TextStyle(
-                color: Colors.white),
-          )
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(15)
-      ),
-      ),
+      //NOTE : if showFloatingButton is true then show container ,i.e, cart or else don't show cart
+      floatingActionButton:showFloatingButton? Container(
+        height: 60,
+        width: 100,
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Text(
+              'Cart',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: Colors.red, borderRadius: BorderRadius.circular(15)),
+      ):SizedBox(),//NOTE: SizedBox and showfloatingButton before container causes cart to disappear when box is not checked
+      body: Center(
+          child: IconButton(
+        icon: Icon(showFloatingButton
+            ? Icons.check_box
+            : Icons.check_box_outline_blank),
+            iconSize: 60,
+            color: Colors.green,
+            onPressed: (){
+          setState((){
+            showFloatingButton=!showFloatingButton;
+          });
+            }
+      )),
     );
   }
 }
