995a0441ed2314f681cfd5ebb699a23c144f417a
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-23 10:50:42 +0530

    Use of focusedBorder() and enabledBorder()

--- lib/screens/textField_Screen.dart
+++ lib/screens/textField_Screen.dart
@@ -15,7 +15,28 @@ class TextFieldScreen extends StatelessWidget {
          child: SizedBox(
            width: 300, //NOTE: it is used for the size of the line,i.e.,text line
            child: TextFormField(
-
+             decoration: const InputDecoration(
+               labelText: 'Name :',
+               labelStyle: TextStyle(
+                 fontSize: 25,
+                 color: Colors.blue,
+                 fontWeight: FontWeight.bold,
+               ),
+               //NOTE:  enabledBorder is used for border
+               enabledBorder: OutlineInputBorder(borderSide: BorderSide(
+                 color: Colors.red,
+                 width: 3.0,
+               ),
+                 borderRadius: BorderRadius.all(Radius.circular(20))
+               ),
+                //NOTE: focusedBorder is used to focus the border
+               focusedBorder: OutlineInputBorder(borderSide: BorderSide(
+                 color: Colors.red,
+                 width: 3.0,
+               ),
+                   borderRadius: BorderRadius.all(Radius.circular(20))
+               ),
+             ),
            ),
          ),
        ),
