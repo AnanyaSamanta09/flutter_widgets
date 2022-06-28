83a23d1aa0841536a693ff7ad1a0d03ce19d8300
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-06-28 09:20:50 +0530

    Random UI of Drawer Screen

--- lib/screens/drawer_Screen.dart
+++ lib/screens/drawer_Screen.dart
@@ -20,22 +20,90 @@ class DrawerScreen extends StatelessWidget {
   }
 }
 
-
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
+
+  custom_tile(Icon icon,String text)
+  {
+    return Row(
+      children: [
+        icon,
+        SizedBox(
+          width: 20,
+        ),
+        Text(text,
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 65,left: 25),
+      padding: const EdgeInsets.only(top: 65, left: 25),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CircleAvatar(
             backgroundImage: AssetImage('assets/images/AR.jfif'),
-            radius: 50,
-          )
+            radius: 40,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text('Ananya Rawat',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                fontSize: 22
+              ),
+          ),
+          Text('ananya123@gmail.com',
+            style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 18
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          custom_tile(Icon(Icons.folder), 'My Files'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.people), 'Shared with me'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.star), 'Starred'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.watch_later_outlined), 'Recent'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.offline_pin), 'Offline'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.upload), 'Uploads'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.backup), 'Backups'),
+          SizedBox(
+            height: 30,
+          ),
+          custom_tile(Icon(Icons.delete), 'Trash'),
+          SizedBox(
+            height: 60,
+          ),
+          custom_tile(Icon(Icons.settings), 'Settings & account'),
         ],
       ),
     );
